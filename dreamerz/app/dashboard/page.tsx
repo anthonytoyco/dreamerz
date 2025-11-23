4a852c9f0e865f6e4036d6a4de4dd00a1344448e
Andrew Cao <andrewcao618@gmail.com>
2025-11-23 17:29:06 +0000

    Added sidebar

--- dreamerz/app/dashboard/page.tsx
+++ dreamerz/app/dashboard/page.tsx
@@ -8,11 +8,7 @@ import {
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
 import { Separator } from '@/components/ui/separator';
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Page() {
   return (
--- dreamerz/components/nav-projects.tsx
+++ dreamerz/components/nav-projects.tsx
@@ -1,14 +1,6 @@
 'use client';
 
 import { type LucideIcon } from 'lucide-react';
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import {
   SidebarGroup,
   SidebarGroupLabel,
