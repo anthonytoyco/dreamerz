acd038d1d34452759f40de37d4cb9155f706dcac
Andrew Cao <andrewcao618@gmail.com>
2025-11-23 17:30:45 +0000

    Deleted unneeded components

--- dreamerz/app/dashboard/page.tsx
+++ dreamerz/app/dashboard/page.tsx
@@ -1,13 +1,4 @@
 import { AppSidebar } from '@/components/app-sidebar';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Page() {
